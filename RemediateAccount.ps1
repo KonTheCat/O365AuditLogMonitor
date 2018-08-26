4d43b2618a8b524051c77520714ef7b51b69efc1
Konstantin <konstantin.kaminskiy@outlook.com>
2018-08-25 22:28:10 -0400

    Update RemediateAccount.ps1

--- RemediateAccount.ps1
+++ RemediateAccount.ps1
@@ -1,29 +1,13 @@
-#this remediates the account using data from O365 flow, Flow calls this via webhook. 
-
 param (
     [object]
     $WebhookData
 )
-    $requestBody = ConvertFrom-Json $WebhookData.RequestBody
-    $domain = $requestBody.domain 
-    $user = $requestBody.user
-    $ID = $requestBody.ID
 
-Write-Output "Management domain, username, and audit log ID to process"
-$domain  
-$user  
-$ID  
-
+#variables
 $ResourceGroup = 'TestO365AuditLogMonitor'
 $StorageAccountName = 'TestO365AuditLogMonitor'
 $TableName = 'AuditLogActionsLog'
-
-#login and such 
 $RemoteCredential = Get-AutomationPSCredential -name 'delegatedadmin'
-$RemoteSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ('https://ps.outlook.com/powershell-liveid?DelegatedOrg=' + $domain) -Credential $RemoteCredential -Authentication Basic -AllowRedirection 
-Import-Module (Import-PSSession $RemoteSession -AllowClobber -DisableNameChecking) -Global -DisableNameChecking
-Connect-MsolService -Credential $RemoteCredential
-$TennantGuid = Get-MsolPartnerContract | Where-Object {$_.DefaultDomainName -eq "$domain"} | Select-Object -ExpandProperty TenantId | Select-Object -ExpandProperty GUID
 
 #Variables for storage  
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection
@@ -31,12 +15,28 @@ Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $
 $context = (Get-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroup).context
 $table = Get-AzureStorageTable -Name $TableName -Context $context
 
+$requestBody = ConvertFrom-Json $WebhookData.RequestBody
+$domain = $requestBody.domain 
+$user = $requestBody.user
+$ID = $requestBody.ID
+
+Write-Output "Securing account $user at $domain, from activity in log $ID"
+
+#login to O365 tennant 
+$RemoteSession = New-PSSession -ConfigurationName Microsoft.Exchange `
+                               -ConnectionUri ('https://ps.outlook.com/powershell-liveid?DelegatedOrg=' + $domain) `
+                               -Credential $RemoteCredential `
+                               -Authentication Basic `
+                               -AllowRedirection
+
+Import-Module (Import-PSSession $RemoteSession -AllowClobber -DisableNameChecking) -Global -DisableNameChecking
+Connect-MsolService -Credential $RemoteCredential
+$TennantGuid = Get-MsolPartnerContract | Where-Object {$_.DefaultDomainName -eq "$domain"} | Select-Object -ExpandProperty TenantId | Select-Object -ExpandProperty GUID
+
 #mailbox actions
 $newPassword = ([System.Web.Security.Membership]::GeneratePassword(16,2))
 Set-MsolUserPassword –UserPrincipalName $user –NewPassword $newPassword -ForceChangePassword $True -TenantId $TennantGuid
-Write-Output "We've set the password for the account $user to be $newPassword. Make sure you record this and share with the user, or be ready to reset the password again. They will have to reset their password on the next logon."
-
-Write-Output "We block the user account and disable owa."
+Write-Output "We've set the password for the account $user to be $newPassword. It will need to be reset on logon."
 Set-MsolUser -UserPrincipalName $user -BlockCredential $true -TenantId $TennantGuid
 Set-CASMailbox -Identity $user -OWAEnabled $false
 
